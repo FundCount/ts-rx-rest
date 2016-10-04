17451c7cd792f9d2d944ac022aeb0ef68a706ceb
Dmitry Drozdov <d.drozdov@fundcount.com>
2016-10-04 17:42:03 +0300

    Implement PUT, POST, DELETE wrappers

--- src/Rest.ts
+++ src/Rest.ts
@@ -1,23 +1,36 @@
 import {Observable} from 'rx';
-import * as Client from 'rest/client/xhr';
+import * as Client from 'rest';
+import * as mime from 'rest/interceptor/mime';
+import * as errorCode from 'rest/interceptor/errorCode';
+
+const rest = Client.wrap(mime, {mime: 'application/json'}).wrap(errorCode);
 
 export default class Rest {
 
-    static doGet<T>(url: string): Observable<T> {
-        return Observable.create<T>(observer => {
-            Client({
-                method: 'GET',
-                path: url,
-                headers: {'Accept': 'application/json'}
-            }).then(
-                (res) => {
-                    observer.onNext(JSON.parse(res.entity));
-                    observer.onCompleted();
-                },
-                (err) => {
-                    observer.onError(err);
-                    observer.onCompleted();
-                })
-        });
+    static doGet<T>(path: string): Observable<T> {
+        return Observable.fromPromise(rest(path)).map(response => response.entity);
+    }
+
+    static doPut<T, R>(path: string, entity: T): Observable<R> {
+        return Observable.fromPromise(rest({
+            path,
+            method: 'PUT',
+            entity
+        })).map(response => response.entity);
+    }
+
+    static doPost<T, R>(path: string, entity: T): Observable<R> {
+        return Observable.fromPromise(rest({
+            path,
+            method: 'POST',
+            entity
+        })).map(response => response.entity);
+    }
+
+    static doDelete(path: string): Observable<any> {
+        return Observable.fromPromise(rest({
+            path,
+            method: 'DELETE'
+        }));
     }
 }
